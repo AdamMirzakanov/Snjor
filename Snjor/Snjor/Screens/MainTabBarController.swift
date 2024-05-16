e798efc85d615ed769274ba13bdb4648efb68d21
Адам <adamirzakan@gmail.com>
2024-05-16 20:48:24 +0300

    feat: pre-configuration of the tab bar controller

---
+++ Snjor/Snjor/Screens/MainTabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  MainTabBarController.swift
+//  Snjor
+//
+//  Created by Адам on 16.05.2024.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemBlue
+  }
+}
