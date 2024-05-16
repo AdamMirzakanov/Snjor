f837346908da0b7edd3300ef1dda409c4b180b09
Адам <adamirzakan@gmail.com>
2024-05-17 00:12:40 +0300

    feat: implement a app factory

---
+++ Snjor/Snjor/Factories/AppFactory.swift
@@ -0,0 +1,29 @@
+//
+//  AppFactory.swift
+//  Snjor
+//
+//  Created by Адам on 17.05.2024.
+//
+
+protocol AppFactoring {
+  func makeMainTabBarCoordinator(
+    _ navigation: Navigable,
+    delegate: MainTabBarCoordinatorDelegate
+  ) -> Coordinatable
+}
+
+struct AppFactory: AppFactoring {
+  // MARK: - Protocol Methods
+  func makeMainTabBarCoordinator(
+    _ navigation: Navigable,
+    delegate: MainTabBarCoordinatorDelegate
+  ) -> Coordinatable {
+    let factory = MainTabBarFactory()
+    let coordinator = MainTabBarCoordinator(
+      navigation: navigation,
+      factory: factory,
+      delegate: delegate
+    )
+    return coordinator
+  }
+}
