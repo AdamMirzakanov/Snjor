eb9967bf0e54ff4cd8799ca0bae46529b4e24c0a
Адам <adamirzakan@gmail.com>
2024-05-16 16:55:49 +0300

    build: initialize Xcode project

---
+++ Snjor/Snjor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Snjor
+//
+//  Created by Адам on 16.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
