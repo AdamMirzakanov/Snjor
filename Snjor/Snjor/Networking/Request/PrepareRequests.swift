20cf0f77350695ecbf0ea08576327092b5916f9b
Адам <adamirzakan@gmail.com>
2024-06-15 19:56:38 +0300

    feat (net): prepare requests

---
+++ Snjor/Snjor/Networking/Request/PrepareRequests.swift
@@ -0,0 +1,71 @@
+//
+//  PrepareRequests.swift
+//  Snjor
+//
+//  Created by Адам on 15.06.2024.
+//
+
+import Foundation
+
+enum PrepareRequests {
+  // MARK: - Private Properties
+  private static var httpMethod: HTTPMethod { .get }
+  private static var scheme: API { .scheme }
+  private static var host: API { .host }
+  private static var accessKey: Authorization { .accessKey }
+
+  // MARK: - Public Methods
+  static func prepareURLRequest(
+    path: String,
+    parameters: Parameters
+  ) throws -> URLRequest {
+    guard let url = prepareURL(
+      from: path,
+      parameters: parameters
+    ) else {
+      throw APIError.URLError
+    }
+
+    var request = URLRequest(url: url)
+    request.allHTTPHeaderFields = prepareHeaders()
+    request.httpMethod = httpMethod.rawValue
+    return request
+  }
+
+  static func prepareHeaders() -> Parameters {
+    var headers: Parameters = [:]
+    headers["Authorization"] = "Client-ID \(accessKey.rawValue)"
+    return headers
+  }
+
+  // MARK: - Private Methods
+  private static func prepareURL(
+    from path: String,
+    parameters: Parameters
+  ) -> URL? {
+    let components = prepareURLComponents(
+      from: path,
+      parameters: parameters)
+    let url = components.url
+    return url
+  }
+
+  private static func prepareURLComponents(
+    from path: String,
+    parameters: Parameters
+  ) -> URLComponents {
+    var components = URLComponents()
+    components.scheme = scheme.rawValue
+    components.host = host.rawValue
+    components.path = path
+    components.queryItems = prepareQueryItems(parameters: parameters)
+    return components
+  }
+
+  private static func prepareQueryItems(parameters: Parameters) -> [URLQueryItem] {
+    parameters.map { URLQueryItem(
+      name: $0.key,
+      value: $0.value)
+    }
+  }
+}
