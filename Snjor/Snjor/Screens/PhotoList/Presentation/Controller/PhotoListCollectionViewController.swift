d7fcdb6c69e8a8e470593ff443328188fb240a56
Адам <adamirzakan@gmail.com>
2024-06-16 13:43:40 +0300

    feat: implement collection update tracking

--- Snjor/Snjor/Screens/PhotoList/Presentation/Controller/PhotoListCollectionViewController.swift
+++ Snjor/Snjor/Screens/PhotoList/Presentation/Controller/PhotoListCollectionViewController.swift
@@ -12,7 +12,7 @@ protocol PhotoListViewControllerDelegate: AnyObject {
   func didSelect()
 }
 
-class PhotoListCollectionViewController: UICollectionViewController {
+class PhotoListCollectionViewController: UICollectionViewController, MessageDisplayable {
   // MARK: - Private Properties
   private(set) var viewModel: any PhotoListViewModelProtocol
   private weak var delegate: (any PhotoListViewControllerDelegate)?
@@ -37,4 +37,26 @@ class PhotoListCollectionViewController: UICollectionViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
   }
+  
+  // MARK: - Private Methods
+  private func stateController() {
+    viewModel
+      .state
+      .receive(on: DispatchQueue.main)
+      .sink { [weak self] state in
+        guard let self = self else { return }
+        switch state {
+        case .success:
+          viewModel.applySnapshot()
+        case .loading:
+          break
+        case .fail(error: let error):
+          self.presentAlert(message: error, title: AppLocalized.error)
+        }
+      }.store(in: &cancellable)
+  }
+}
+
+enum AppLocalized {
+  static let error = "Error"
 }
