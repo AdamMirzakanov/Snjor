3e9f09cc8ca77e3ca3dd5fa9caff5090cc67d702
Adam Mirzakanov <adamirzakan@gmail.com>
2024-08-18 14:46:55 +0300

    feat: prepare the cell of the photo contained in the album

--- Snjor/Snjor/Screens/AlbumPhotos/Presentation/Views/Cell/AlbumPhotoCell.swift
+++ Snjor/Snjor/Screens/AlbumPhotos/Presentation/Views/Cell/AlbumPhotoCell.swift
@@ -5,4 +5,49 @@
 //  Created by Адам Мирзаканов on 18.08.2024.
 //
 
-import Foundation
+import UIKit
+
+protocol AlbumPhotoCellDelegate: AnyObject {
+  func downloadTapped(_ cell: AlbumPhotoCell)
+}
+
+final class AlbumPhotoCell: UICollectionViewCell {
+  
+  // MARK: - Delegate
+  weak var delegate: (any AlbumPhotoCellDelegate)?
+
+  // MARK: - Main View
+  let mainView: AlbumPhotoCellMainView = {
+    return $0
+  }(AlbumPhotoCellMainView())
+  
+  // MARK: - Initializers
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setupMainView()
+    mainView.delegate = self
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  // MARK: - Override Methods
+  override func prepareForReuse() {
+    super.prepareForReuse()
+    mainView.prepareForReuse()
+  }
+
+  // MARK: - Setup Data
+  func configure(viewModelItem: AlbumPhotosViewModelItem) {
+    let photo = viewModelItem.photo
+    let photoURL = viewModelItem.photoURL
+    mainView.configure(with: photo, url: photoURL)
+  }
+
+  // MARK: - Setup Views
+  private func setupMainView() {
+    contentView.addSubview(mainView)
+    mainView.fillSuperView()
+  }
+}
--- Snjor/Snjor/Screens/AlbumPhotos/Presentation/Views/Cell/Extensions/AlbumPhotoCell + AlbumPhotoCellMainViewDelegate.swift
+++ Snjor/Snjor/Screens/AlbumPhotos/Presentation/Views/Cell/Extensions/AlbumPhotoCell + AlbumPhotoCellMainViewDelegate.swift
@@ -5,4 +5,9 @@
 //  Created by Адам Мирзаканов on 18.08.2024.
 //
 
-import Foundation
+extension AlbumPhotoCell: AlbumPhotoCellMainViewDelegate {
+  func downloadTapped() {
+    guard let delegate = delegate else { return }
+    delegate.downloadTapped(self)
+  }
+}
