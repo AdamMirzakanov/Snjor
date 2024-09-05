43c83541f1a45db482419bbbf65fb9ebf398539f
Adam Mirzakanov <adamirzakan@gmail.com>
2024-09-05 15:27:14 +0300

    feat (ui): increase the rounding of the tag cell

--- Snjor/Snjor/Screens/SearchScreen/Presentation/Views/Segments/Topics & Albums/Albums/Tags/Views/Cell/TagCellConst.swift
+++ Snjor/Snjor/Screens/SearchScreen/Presentation/Views/Segments/Topics & Albums/Albums/Tags/Views/Cell/TagCellConst.swift
@@ -10,5 +10,5 @@ import CoreGraphics
 enum TagCellConst {
   static let tagLabelOpacity: CGFloat = 0.6
   static let tagFontSize: CGFloat = 11.0
-  static let tagLabelCornerRadius: CGFloat = 8.0
+  static let tagLabelCornerRadius: CGFloat = 11.0
 }
