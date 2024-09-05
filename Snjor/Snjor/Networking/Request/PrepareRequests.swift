42d3631b11645a55c696fe9c46fba5afa3ccc2a7
Adam Mirzakanov <adamirzakan@gmail.com>
2024-09-05 15:04:18 +0300

    feat (net): change authorization keys with each request

--- Snjor/Snjor/Networking/Request/PrepareRequests.swift
+++ Snjor/Snjor/Networking/Request/PrepareRequests.swift
@@ -12,7 +12,7 @@ enum PrepareRequests {
   private static var getHTTP: HTTPMethod { .get }
   private static var scheme: API { .scheme }
   private static var host: API { .host }
-  private static var accessKey: Authorization { .accessKey }
+//  private static var accessKey: Authorization { .accessKey }
 
   // MARK: Internal Methods
   static func prepareTopicsTitleAPIRequest(topics: String) throws -> URLRequest {
@@ -122,7 +122,7 @@ enum PrepareRequests {
 
   private static func prepareHeaders() -> Parameters {
     var headers: Parameters = [:]
-    headers["Authorization"] = "Client-ID " + accessKey.rawValue
+    headers["Authorization"] = "Client-ID " + Authorization.accessKey
     return headers
   }
 }
--- Snjor/Snjor/Networking/URLComponents/Authorization.swift
+++ Snjor/Snjor/Networking/URLComponents/Authorization.swift
@@ -5,15 +5,18 @@
 //  Created by Адам on 15.06.2024.
 //
 
-enum Authorization: String {
-  case accessKey = "Ce9XSW39p8HVwa98GInAYBURSalesGqzX1ZAjfQPdio"
+enum Authorization {
+  static var accessKey: String {
+    let keys = [
+      "qWxk9iN9WGD2gnCZnQuvC34hNqUDWGPr5bV_dLRC89s",
+      "rcKlvKOv4ZNiLed47VZnync8ynWaNDNYw0MxZ5Tqiko",
+      "gPoLLPuPfcL6D6GBbc48sOyLuLD9z2CFbdVNaZ8juFE",
+      "Ce9XSW39p8HVwa98GInAYBURSalesGqzX1ZAjfQPdio",
+      "gLMW-rd9s9p_Rg5dNG9C4rHQec6v1M9XHao5awwxFno",
+      "r6w7Z9ZngliQ2zpcveM-wtWeAKF_urQWYP1evbOu6ys",
+      "cPT2fqhm-IU6GJYAvkSoChMFKd6cB1R9J9fho71HbK4",
+      "J_rWahdyRoNQS2PxNatW5yF2-65ynb5zXQU1sRblerE"
+    ]
+    return keys.randomElement() ?? .empty
+  }
 }
-
-// qWxk9iN9WGD2gnCZnQuvC34hNqUDWGPr5bV_dLRC89s
-// rcKlvKOv4ZNiLed47VZnync8ynWaNDNYw0MxZ5Tqiko
-// gPoLLPuPfcL6D6GBbc48sOyLuLD9z2CFbdVNaZ8juFE
-// Ce9XSW39p8HVwa98GInAYBURSalesGqzX1ZAjfQPdio 🇳🇴
-// gLMW-rd9s9p_Rg5dNG9C4rHQec6v1M9XHao5awwxFno
-// r6w7Z9ZngliQ2zpcveM-wtWeAKF_urQWYP1evbOu6ys
-// cPT2fqhm-IU6GJYAvkSoChMFKd6cB1R9J9fho71HbK4
-// J_rWahdyRoNQS2PxNatW5yF2-65ynb5zXQU1sRblerE
