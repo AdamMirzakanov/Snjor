b69c52c38dfa3ce4cc17b984820e67259463507b
Adam Mirzakanov <adamirzakan@gmail.com>
2024-09-06 16:21:06 +0300

    fix (net): eliminate the cycle of strong links during page loading

--- Snjor/Snjor/Networking/Paging/LastPageValidationUseCase.swift
+++ Snjor/Snjor/Networking/Paging/LastPageValidationUseCase.swift
@@ -5,62 +5,28 @@
 //  Created by Адам on 15.06.2024.
 //
 
-import Foundation
-
-final class LastPageValidationUseCase: LastPageValidationUseCaseProtocol {
-  // MARK: Private Properties
-  private let loadQueue = DispatchQueue(label: .loadQueue, qos: .userInitiated)
-  private let threshold = LastPageValidationUseCaseConst.thresholdValue
-  private(set) var lastPage = false
-  private var isLoading = false
-  private var loadWorkItem: DispatchWorkItem?
+struct LastPageValidationUseCase: LastPageValidationUseCaseProtocol {
   
+  // MARK: Private Properties
+  private var threshold: Int = LastPageValidationUseCaseConst.thresholdValue
+  private(set) var lastPage = false
+
   // MARK: Internal Methods
-  func updateLastPage(itemsCount: Int) {
-    if itemsCount < threshold {
-      lastPage = true
+  mutating func updateLastPage(itemsCount: Int) {
+    if itemsCount == Int.zero {
+      lastPage = false
     }
   }
-  
+
   func checkAndLoadMoreItems(
     at itemIndex: Int,
     actualItems: Int,
-    action: @escaping () -> Void
+    action: () -> Void
   ) {
-    guard
-      !lastPage,
-      !isLoading,
-      itemIndex >= actualItems - threshold
-    else {
-      return
-    }
-    loadWorkItem?.cancel()
-    isLoading = true
-    loadWorkItem = DispatchWorkItem { [weak self] in
-      guard let self = self else { return }
-      self.startLoading(action: action)
-    }
-    if let loadWorkItem = loadWorkItem {
-      loadQueue.asyncAfter(
-        deadline: .now() + .loadDelay,
-        execute: loadWorkItem
-      )
+    guard !lastPage else { return }
+    let limit = actualItems - threshold
+    if limit == itemIndex {
+      action()
     }
   }
-
-  // MARK: Private Methods
-  private func startLoading(action: @escaping () -> Void) {
-    action()
-    self.isLoading = false
-  }
-}
-
-// MARK: - Double
-private extension Double {
-  static let loadDelay = 0.3
-}
-
-// MARK: - String
-private extension String {
-  static let loadQueue = "loadQueue"
 }
