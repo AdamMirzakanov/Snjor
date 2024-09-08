9ca896ce5bda8c819c36dee604f0bd59506cb29f
Adam Mirzakanov <adamirzakan@gmail.com>
2024-09-09 00:33:34 +0300

    feat (ui): move the left edge of the user

--- Snjor/Snjor/Screens/SearchScreen/Presentation/Views/Segments/Users/Views/Cell/CellMainView/UserTableViewCellMainViewConst.swift
+++ Snjor/Snjor/Screens/SearchScreen/Presentation/Views/Segments/Users/Views/Cell/CellMainView/UserTableViewCellMainViewConst.swift
@@ -11,7 +11,7 @@ enum UserTableViewCellMainViewConst {
   static let topPadding: CGFloat = 10.0
   static let rightPadding: CGFloat = 10.0
   static let bottomPadding: CGFloat = 10.0
-  static let leftPadding: CGFloat = 10.0
+  static let leftPadding: CGFloat = 20.0
   static let stackViewSpacing: CGFloat = 10.0
   static let mainImageViewCornerRadius: CGFloat = 30.0
   static let mainImageViewSize: CGFloat = 60.0
