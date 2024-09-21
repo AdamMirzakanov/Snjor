8c03ed3dba02f7d2b48f82f6b500196701fc81f2
Adam Mirzakanov <adamirzakan@gmail.com>
2024-09-22 02:09:59 +0300

    feat: remove the gray selection when clicking on a row in the table displaying users

--- Snjor/Snjor/Screens/SearchScreen/Presentation/Controller/Extensions/SearchScreenViewController + UITableViewDelegate.swift
+++ Snjor/Snjor/Screens/SearchScreen/Presentation/Controller/Extensions/SearchScreenViewController + UITableViewDelegate.swift
@@ -12,6 +12,7 @@ extension SearchScreenViewController: UITableViewDelegate {
     _ tableView: UITableView,
     didSelectRowAt indexPath: IndexPath
   ) {
+    tableView.deselectRow(at: indexPath, animated: true)
     guard let delegate = delegate else { return }
     let user = usersViewModel.getItem(at: indexPath.row)
     delegate.userRowDidSelect(user)
