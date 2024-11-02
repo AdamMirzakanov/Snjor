1c2d16b60b77547687cb5ebc57fbbc58ce374e48
Adam Mirzakanov <adamirzakan@gmail.com>
2024-11-02 17:02:14 +0300

    feat (ui): make minor edits to the overall appearance of the application

--- Snjor/Snjor/Screens/MainTabBar/MainTabBarControllerConst.swift
+++ Snjor/Snjor/Screens/MainTabBar/MainTabBarControllerConst.swift
@@ -14,7 +14,7 @@ enum MainTabBarControllerConst {
   static let searchButtonTag: Int = 1
   static let settingsButtonTag: Int = 2
   static let pressingScale: CGFloat = 0.6
-  static let duration: CGFloat = 0.12
+  static let duration: CGFloat = 0.1
   static let defaultOpacity: CGFloat = 0.4
   static let maxOpacity: CGFloat = 1.0
   static let photoListButtonTag: Int = 0
@@ -22,11 +22,11 @@ enum MainTabBarControllerConst {
   static let gradientViewYPosition: CGFloat = 150.0
   static let firstColorLocation: CGFloat = 0.05
   static let secondColorLocation: CGFloat = 0.7
-  static let buttonSize: CGFloat = 25.0
-  static let customBarHorizontalInset: CGFloat = 70.0
-  static let customBarVerticalInset: CGFloat = 90.0
-  static let customBarWidthOffset: CGFloat = 140.0
-  static let customBarHeight: CGFloat = 55.0
+  static let buttonSize: CGFloat = 23.0
+  static let customBarVerticalInset: CGFloat = 95.0
+  static let customBarHorizontalInset: CGFloat = 90.0
+  static let customBarWidthOffset: CGFloat = customBarHorizontalInset * 2
+  static let customBarHeight: CGFloat = 50.0
   static let photosButtonIcon: String = "photos"
   static let searchButtonIcon: String = "search"
   static let settingsButtonIcon: String = "slider"
--- Snjor/Snjor/Screens/PageScreen/Presentation/Views/RootView/PageScreenRootViewConst.swift
+++ Snjor/Snjor/Screens/PageScreen/Presentation/Views/RootView/PageScreenRootViewConst.swift
@@ -9,12 +9,12 @@ import CoreGraphics
 
 enum PageScreenRootViewConst {
   static let gradientStartLocation: CGFloat = 0.11
-  static let gradientEndLocation: CGFloat = 0.37
+  static let gradientEndLocation: CGFloat = 0.27
   static let gradientOpacity: CGFloat = 1.0
   static let topicsCollectionViewHeight: CGFloat = 40.0
   static let appNameLabelFontSize: CGFloat = 20.0
   static let appNameLabelTextKern: CGFloat = 2.5
   static let topicsCollectionViewTopAnchor: CGFloat = 15.0
-  static let gradientViewTopAnchor: CGFloat = -15.0
+  static let gradientViewTopAnchor: CGFloat = 0.0
   static let appNameLabelTopAnchor: CGFloat = -25.0
 }
